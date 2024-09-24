f403c237c6e9dba621736d5aa3624d688ea2a989
Mason Ballengee <masaball@iu.edu>
2024-09-24 16:03:25 -0400

    Implement authorization_code flow

--- lib/browse_everything/driver/sharepoint.rb
+++ lib/browse_everything/driver/sharepoint.rb
@@ -30,15 +30,17 @@ module BrowseEverything
       def validate_config
         raise InitializationError, 'Sharepoint driver requires a :client_id argument' unless config[:client_id]
         raise InitializationError, 'Sharepoint driver requires a :client_secret argument' unless config[:client_secret]
-        raise InitializationError, 'Sharepoint driver requires a :domain argument' unless config[:domain]
-        raise InitializationError, 'Sharepoint driver requires a :site_name argument' unless config[:site_name]
+        raise InitializationError, 'Sharepoint driver requires a :tenant_id argument' unless config[:tenant_id]
+        raise InitializationError, 'Sharepoint driver requires a :redirect_uri argument' unless config[:redirect_uri]
+        raise InitializationError, 'Sharepoint driver requires a :scope argument' unless config[:scope]
       end
 
       # Retrieves the file entry objects for a given path to MS-graph drive resource
       # @param [String] id of the file or folder
       # @return [Array<BrowseEverything::FileEntry>]
       def contents(id = '')
-        sharepoint_session
+        token_refresh if authorized?
+
         folder = id.empty? ? drives : items_by_id(id)
         values = []
 
@@ -50,56 +52,76 @@ module BrowseEverything
         @sorter.call(@entries)
       end
 
-      # Not used as we currently only deal with Client Credentials flow
       # @return [String]
       # Authorization url that is used to request the initial access code from Sharepoint/Onedrive/365/etc
       def auth_link(*_args)
-        Addressable::URI.parse("https://login.microsoftonline.com/kingsfund.org.uk/oauth2/v2.0/authorize")
+        Addressable::URI.parse("https://login.microsoftonline.com/#{config[:tenant_id]}/oauth2/v2.0/authorize?#{auth_query_string}")
       end
 
       # @return [Boolean]
       def authorized?
-        unless @token.present?
-          authorize!
-        end
         @token.present?
       end
 
       def authorize!
-        # TODO handle other authentication strategies (other than client_credentials)
-        register_access_token(sharepoint_session.get_access_token)
+        return if @code.blank?
+        register_access_token(sharepoint_session.get_access_token(@code))
+        @code = nil
+        @token
+      end
+
+      def connect(params, _data, _url_options)
+        @code = params[:code]
+        authorize!
       end
 
       # @param [String] id of the file on MS graph drive
       # @return [Array<String, Hash>]
       def link_for(id)
-         file = items_by_id(id)
-         extras = {file_name: file['name'], file_size: file['size'].to_i}
-         [download_url(file), extras]
+        file = items_by_id(id)
+        extras = {file_name: file['name'], file_size: file['size'].to_i}
+        [download_url(file), extras]
       end
-
 
       private
 
-      def token_expired?
-        return true if expiration_time.nil?
-        Time.now.to_i > expiration_time
-      end
+      def auth_query_string
+        query = []
+        # keep_if deletes from and returns self, so dup config to not overwrite original
+        base = config.dup.keep_if { |k,v| ['client_id', 'scope', 'redirect_uri'].include?(k) }
+        base.each do |k,v|
+          query += ["#{k}=#{v}"]
+        end
+        query += ["response_type=code"]
 
+        query.join('&')
+      end
 
       def session
         AuthenticationFactory.new(
           self.class.authentication_klass,
           client_id: config[:client_id],
           client_secret: config[:client_secret],
-          access_token: sharepoint_token,
-          domain: config[:domain],
-          site_name: config[:site_name],
+          tenant_id: config[:tenant_id],
+          scope: config[:scope],
+          redirect_uri: config[:redirect_uri],
+          code: @code.presence,
+          access_token: @token.presence
         )
       end
 
       def authenticate
         session.authenticate
+      end
+
+      def sharepoint_session
+        @sharepoint_session ||= authenticate
+      end
+
+      def token_refresh
+        return @token unless token_expired?
+
+        register_access_token(sharepoint_session.refresh_token)
       end
 
       # If there is an active session, {@token} will be set by {BrowseEverythingController} using data stored in the
@@ -108,9 +130,11 @@ module BrowseEverything
       # @param [OAuth2::AccessToken] access_token
       def register_access_token(access_token)
         @token = {
-          'token' => access_token.token,
-          'expires_at' => access_token.expires_at
-        }
+                   'token' => access_token.token,
+                   'expires_in' => access_token.expires_in,
+                   'expires_at' => access_token.expires_at,
+                   'refresh_token' => access_token.refresh_token
+                 }
       end
 
       def sharepoint_token
@@ -121,6 +145,25 @@ module BrowseEverything
       def expiration_time
         return unless @token
         @token.fetch('expires_at', nil).to_i
+      end
+
+      def token_expired?
+        return true if expiration_time.nil?
+        Time.now.to_i > expiration_time
+      end
+
+      def sharepoint_request(sharepoint_uri)
+        @auth = "Bearer " + sharepoint_token
+
+        uri = URI.parse(sharepoint_uri)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if uri.scheme == 'https'
+
+        response = http.start do
+          request = Net::HTTP::Get.new(uri.request_uri,{'Authorization' => @auth})
+          http.request(request)
+        end
+        JSON.parse(response.body)
       end
 
       # Constructs a BrowseEverything::FileEntry object for a Sharepoint file
@@ -145,54 +188,22 @@ module BrowseEverything
         !file['file'].present?
       end
 
-     ##################################################################
-     # The below are all candidates to go its own sharepoint api module
-     # or some such
-     ##################################################################
-
-      def sharepoint_request(sharepoint_uri)
-        sharepoint_client
-        @auth = "Bearer "+sharepoint_token
-
-        uri = URI.parse(sharepoint_uri)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true if uri.scheme == 'https'
-
-        response = http.start do
-          request = Net::HTTP::Get.new(uri.request_uri,{'Authorization' => @auth})
-          http.request(request)
-        end
-        JSON.parse(response.body)
-      end
-
-      def site_id
-        @site_id ||= sharepoint_request("https://graph.microsoft.com/v1.0/sites/#{config[:domain]}:/sites/#{config[:site_name]}/")['id']
-      end
+      # def site_id
+      #   @site_id ||= sharepoint_request("https://graph.microsoft.com/v1.0/sites/#{config[:domain]}:/sites/#{config[:site_name]}/")['id']
+      # end
 
       def drives
-       @drives ||= sharepoint_request("https://graph.microsoft.com/v1.0/sites/#{site_id}/drives")['value']
-     end
-
-     def items_by_id(id)
-       item = sharepoint_request("https://graph.microsoft.com/v1.0/sites/#{site_id}/drives/#{id}")
-       item['value'].present? ? item['value'] : item
-     end
-
-     def download_url(file)
-       file['@microsoft.graph.downloadUrl']
-     end
-
-    def sharepoint_client
-      if token_expired?
-        session = sharepoint_session
-        register_access_token(sharepoint_session.get_access_token)
+        @drives ||= sharepoint_request("https://graph.microsoft.com/v1.0/me/drives")['value']
       end
-    end
 
-    def sharepoint_session
-      authenticate
-     end
+      def items_by_id(id)
+        item = sharepoint_request("https://graph.microsoft.com/v1.0/me/drives/#{id}")
+        item['value'].present? ? item['value'] : item
+      end
 
+      def download_url(file)
+        file['@microsoft.graph.downloadUrl']
+      end
     end
   end
 end
